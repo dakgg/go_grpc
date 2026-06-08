d1917ca41bbc84f950834f538432785eac66b650
dakgg <hhs1992@naver.com>
2026-06-09 01:54:42 +0900

    ho

--- client/Assets/Scripts/AuthService.cs
+++ client/Assets/Scripts/AuthService.cs
@@ -1,17 +1,24 @@
 
 
 using System.Threading.Tasks;
+using Cysharp.Net.Http;
 using Dakg;
 using Grpc.Net.Client;
 
 public class AuthService
 {
 
+    GrpcChannel _channel;
     Dakg.GameService.GameServiceClient _client;
 
     public void Connect()
     {
-        _client = new(GrpcChannel.ForAddress("https://127.0.0.1:50051"));
+        _channel = GrpcChannel.ForAddress("http://127.0.0.1:50051", new GrpcChannelOptions
+        {
+            HttpHandler = new YetAnotherHttpHandler { Http2Only = true },
+            DisposeHttpClient = true
+        });
+        _client = new(_channel);
     }
 
     public async Task<LoginResponse> Login()
